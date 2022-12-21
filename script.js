351a39ff9225a75d2906c74e4bc97f2e8adf8fb6
Raihan Zulfi <114371959+raizulfi@users.noreply.github.com>
2022-12-22 00:19:14 +0700

    ‎

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Add your answer here
+$('.text-contents').hide()
+
+$('.lesson').hover(
+
+	function() {
+		$(this).find('.text-contents').show()
+	},
+    
+	function() {
+		$(this).find('.text-contents').hide()
+	}
+) 
